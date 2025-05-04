e9dd7965348aec4f729fbd70adb2340bce8aad33
IsraelHub <israelcunhaolive@gmail.com>
2025-05-04 07:29:55 -0300

    🎉 chore: initial commit with basic React/TypeScript project configuration

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: 'localhost',
+    },
+    proxy: {},
+    cors: true,
+    // Adicionando o host do ngrok à lista de hosts permitidos
+    allowedHosts: [
+      '0ff3-2804-2370-ba00-962-843a-1433-75f5-c0fb.ngrok-free.app',
+      '.ngrok-free.app', // Permitir todos os subdomínios do ngrok
+      'localhost'
+    ],
+  },
+})
